307595c0a5d9b3915e6a3d982b9fedc34b77c7ab
lokesh88 <mm.lokesh.tiwari@gmail.com>
2013-06-16 14:11:51 +0530

    sample app dir structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MembershipProfilesSample::Application.config.secret_token = 'b0ca5a25281cadd8ffd1264b91bba5bbf85ce1e5123e3793510aee879afdcf94c50fc5dc6fc613012f6ae4a1bf696f6597589f6cbd8014a26ab20fdcd996b427'
